a721750663fca38bb9faa63a345ffce11da1977a
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-04-02 10:42:27 -0500

    Update ingredients, shopping-list, display details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cookbook';
+  loadedFeature: 'recipe';
+  onNavigate(feature  ) {
+    this.loadedFeature = feature
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Toast', 'Toasted bread', 'https://cdn.pixabay.com/photo/2015/12/05/11/04/bread-1077984_1280.jpg'),
-    new Recipe('Toast', 'Toasted bread', 'https://cdn.pixabay.com/photo/2015/12/05/11/04/bread-1077984_1280.jpg')
+    new Recipe('More Toast', 'Toasted bread', 'https://cdn.pixabay.com/photo/2015/12/05/11/04/bread-1077984_1280.jpg')
 
   ];
 
@@ -18,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
