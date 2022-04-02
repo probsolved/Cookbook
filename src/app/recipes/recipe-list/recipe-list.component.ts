52cc95878b62d581410f00a740485877ea69e15c
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-04-01 23:01:50 -0500

    create and style initial components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Toast', 'Toasted bread', 'https://cdn.pixabay.com/photo/2015/12/05/11/04/bread-1077984_1280.jpg'),
+    new Recipe('Toast', 'Toasted bread', 'https://cdn.pixabay.com/photo/2015/12/05/11/04/bread-1077984_1280.jpg')
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
