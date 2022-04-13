823c6ab8228228745919412bb02eced57c25bb43
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-04-13 17:34:12 -0500

    Add Services, Directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,19 +8,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Toast', 'Toasted bread', 'https://cdn.pixabay.com/photo/2015/12/05/11/04/bread-1077984_1280.jpg'),
-    new Recipe('More Toast', 'Toasted bread', 'https://cdn.pixabay.com/photo/2015/12/05/11/04/bread-1077984_1280.jpg')
+  recipes: Recipe[];
 
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
 }
