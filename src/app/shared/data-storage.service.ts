7bcc59344e75bd245cd431a5d85946c04314e8f2
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-04-26 14:26:11 -0500

    resolve data before loading

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 
@@ -17,15 +17,18 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
     .get<Recipe[]>('https://cookbook-4a089-default-rtdb.firebaseio.com/recipes.json')
     .pipe(map(recipes => {
       return recipes.map(recipe => {
         return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
       });
-    }))
-    .subscribe(recipes => {
-      this.recipeService.setRecipes(recipes);
     })
+    ,tap(recipes => {
+            this.recipeService.setRecipes(recipes);
+
+    })
+    )
+
   }
 }
